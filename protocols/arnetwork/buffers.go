3c7b5b7c4301f6efb233ad15193b21359aca8e0e
Kent Rancourt <kent.rancourt@gmail.com>
2018-11-15 00:56:42 -0500

    implement arnetwork ping/pong

--- protocols/arnetwork/buffers.go
+++ protocols/arnetwork/buffers.go
@@ -19,6 +19,41 @@ func NewBuffers(
 	d2cInChs := map[uint8]chan<- Frame{}
 	d2cOutChs := map[uint8]<-chan Frame{}
 
+	// TODO: This is a GUESS at how this buffer should be configured. The details
+	// of this buffer are not well documented.
+	pingBuf := newD2CBuffer(
+		D2CBufferConfig{
+			ID:            0,
+			FrameType:     arnetworkal.FrameTypeData,
+			Size:          20,
+			MaxDataSize:   8, // This is the size of the "timespec" data we receive
+			IsOverwriting: true,
+		},
+	)
+	// This is for arnetwork internal use only. We'll add the input channel of
+	// this d2cBuffer to d2cInChs so that receiveFrames(...) can add frames to
+	// this buffer, BUT we'll refrain from adding the output channel of this
+	// d2cBuffer to d2cOutChs, because those channels are returned from this
+	// function and can be listened to by the caller-- which we do not want in
+	// this case.
+	d2cInChs[pingBuf.ID] = pingBuf.inCh
+
+	// TODO: This is a GUESS at how this buffer should be configured. The
+	// details of this buffer are not well documented.
+	pongBuf := newC2DBuffer(
+		C2DBufferConfig{
+			ID:            1,
+			FrameType:     arnetworkal.FrameTypeData,
+			Size:          20,
+			MaxDataSize:   8, // This is the size of the "timespec" data we must echo
+			IsOverwriting: true,
+		},
+		frameSender,
+	)
+	// The above is for arnetwork internal use only. We won't add its input
+	// channel to c2dInChs, because those channels are returned from this function
+	// and can be written to by the caller-- which we do not want in this case.
+
 	for _, bufCfg := range c2dBufCfgs {
 		if err := bufCfg.validate(); err != nil {
 			return nil, nil, err
@@ -69,11 +104,32 @@ func NewBuffers(
 		}
 	}
 
+	// Mux received frames into the appropriate buffers
 	go receiveFrames(frameReceiver, d2cInChs)
+
+	// Respond to pings. This turns out to be very important for avoiding
+	// disconnects! Why? The arnetwork protocol (on the device end) assumes a
+	// disconnect has occurred after five seconds of receiving no data from the
+	// controller. Once that occurs, the device stops sending data as well. At
+	// that point, neither controller or device is speaking to the other and the
+	// disconnect is now real-- a self-fulfilling prophecy!
+	//
+	// Since we can't guarantee that a user of this SDK even HAS data
+	// to send more often than every five seconds, our best bet for avoiding
+	// disconnects is to simply respond to pings.
+	go func() {
+		for frame := range pingBuf.buffer.outCh {
+			log.Debug("received ping; sending pong")
+			pongBuf.inCh <- Frame{
+				Data: frame.Data,
+			}
+		}
+	}()
 
 	return c2dInChs, d2cOutChs, nil
 }
 
+// receiveFrames muxes frames into the appropriate buffers.
 func receiveFrames(
 	frameReceiver arnetworkal.FrameReceiver,
 	d2cInChs map[uint8]chan<- Frame,
